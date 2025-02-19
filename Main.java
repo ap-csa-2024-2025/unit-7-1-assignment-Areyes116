393fa66891c0a4a842614f21a7cb44c9ba0096b1
Areyes116 <adrian.reyes2006@icloud.com>
2025-02-19 02:33:04 +0000

    Arise

--- Main.java
+++ Main.java
@@ -3,14 +3,29 @@ import java.util.Scanner;
 
 public class Main
 {
-  public static void main(String[] args)
-  {
-    Scanner sc = new Scanner(System.in);
-    String input = "";
-
-    while (!input.equals("STOP"))
+    public static void main(String[] args)
     {
-      System.out.println("infinite loop! replace with your code");
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> words = new ArrayList<>();
+        
+        System.out.println("Please enter words, enter STOP to stop the loop.");
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("STOP")) {
+                break;
+            }
+            words.add(input);
+        }
+        
+        System.out.println(words.size());
+        System.out.println(words);
+        
+        if (words.size() > 2)
+        {
+            words.set(words.size() - 1, words.get(0)); // Replace last index with first
+            words.remove(0); // Remove first index
+        }
+        
+        System.out.println(words);
     }
-  }
 }
